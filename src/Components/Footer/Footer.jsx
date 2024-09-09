82d961490d61f4cd79e4fd5a90f5b1ad10317426
AhmedSaeed <ahmedsaeedfci@gmail.com>
2024-09-09 22:33:41 +0300

    Modieied index.css and created Footer Component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Footer.css'
+import openWeather from '../../assets/images/openweather.png'
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className="container">
+                <p>Copyright 2024&copy; <a target='_blank' href="https://a-saeed.netlify.app/">Ahmed Saeed.</a> All Rights Reserved</p>
+                <div className="openweather-rights">
+                    <span>Powered By</span>
+
+                    <img src={openWeather} alt="open weather image" />
+
+                    <span>Design By <a href="https://x.com/codewithsadee">Sadee</a></span>
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Footer;
